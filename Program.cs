cf43df68fdfe08027f1b318818c85ae4570f8198
Ryan Funk <77073464+RyanFunk2000@users.noreply.github.com>
2024-06-22 23:48:46 -0600

    add privacy policy

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using game_trends_explorer.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add configuration to read from environment variables
-var connectionString = builder.Configuration.GetConnectionString("GameTrendContext");
+var connectionString = builder.Configuration.GetValue<string>("GameTrendContext");
 
 // Read connection string from environment variable
 builder.Services.AddDbContext<GameTrendContext>(options =>
